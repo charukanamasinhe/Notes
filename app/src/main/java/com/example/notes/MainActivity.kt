23a8e96a84787cb48fec5168aa844c192f2c4b6e
charukanamasinhe <charukawnp@gmail.com>
2024-05-12 19:53:17 +0530

    change

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.notes
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Telephony.Sms.Intents
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.ReturnThis
 import androidx.appcompat.app.AppCompatActivity
@@ -13,16 +14,16 @@ import com.example.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+//    private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
 
-        binding.addButton.setOnClickListener{
+        val start: Button =findViewById(R.id.startedbtn)
+
+        start.setOnClickListener{
             val intent = Intent(this,AddnotesActivity::class.java)
             startActivity(intent)
 
